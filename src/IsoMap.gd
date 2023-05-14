d0a7f800097ef0faa2985ef4b1bccb50368eab3c
Meceke <113243336+Meceke@users.noreply.github.com>
2023-05-14 19:00:08 +0200

    creating project
    created project and added tileMap

---
+++ src/IsoMap.gd
@@ -0,0 +1,12 @@
+extends TileMap
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_cell(0, Vector2i(0, 0),0, Vector2(0, 0),0)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
